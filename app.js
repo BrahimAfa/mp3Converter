6fcb3f3149ef91342a3a0234f3cffcf6f1127ec4
Brahim AFASSY <brahimafassy@gmail.com>
2020-07-30 03:59:02 +0100

    trying quite state

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const app = express();
 const execa = require("execa");
-const { stdout, stderr } = require("process");
-
-var exec = require('child_process').exec
 
 app.get('/:list',async (req,res)=>{
     try{
-         // execa('youtube-dl',["-x", "--print-json" ,"--no-warnings", "--audio-format","mp3","https://www.youtube.com/playlist?list=PLETIo5u_JSiOrahkO8xY5tvzh8O4PLJA1","|","jq","-r",".title"])
-         exec('youtube-dl -x --print-json --no-warnings --audio-format mp3 https://www.youtube.com/playlist?list=PLETIo5u_JSiOrahkO8xY5tvzh8O4PLJA1 | jq -r .title',(err,stdout,stderr)=>{
-             if(err) return console.log(err.message);
-             if(stderr) return console.error(stderr);
-             console.log(stdout);
-         })
-         
+         await execa('youtube-dl',["-x", "-e" ,"--no-warnings", "--audio-format","mp3","https://www.youtube.com/playlist?list=PLETIo5u_JSiOrahkO8xY5tvzh8O4PLJA1"]).stdout.pipe(process.stdout);
          res.status(200).send("END");
     }catch(ex){
         console.log(ex);
-        res.status(400).send(ex.message);
+        res.status(400).json(ex.message);
     }
 });
 
