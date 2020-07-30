bdeae2058b829747fdf81929ee1064670377b1fb
Brahim AFASSY <brahimafassy@gmail.com>
2020-07-30 03:37:25 +0100

    spilling : fixed staus to status

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const execa = require("execa");
 app.get('/:list',async (req,res)=>{
     try{
          execa('youtube-dl',["-x", "--print-json" ,"--no-warnings", "--audio-format","mp3","https://www.youtube.com/playlist?list=PLETIo5u_JSiOrahkO8xY5tvzh8O4PLJA1","|","jq","-r",".title"]).stdout.pipe(process.stdout);
-         res.staus(200).send("END");
+         res.status(200).send("END");
     }catch(ex){
         console.log(ex);
-        res.staus(400).json(ex.message);
+        res.status(400).send(ex.message);
     }
 });
 
