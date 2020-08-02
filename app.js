bf63dcdd9c2067b4df56b7075c164018fe288ec3
Brahim AFASSY <brahimafassy@gmail.com>
2020-08-02 20:33:58 +0100

    added filters max and end for playlist

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.get('/video/:id',async (req,res)=>{
     output.on('close', async () => {
         // when the archive finish zipping the folder
         // send the file to the client to download it 
-        await execa('rm', ['-rf','./mp3']);
         console.log('archiver has been finalized and the output file descriptor has closed.');
         res.redirect('/download?type=video');
     });
@@ -64,10 +63,16 @@ app.get('/playlist/:id',async (req,res)=>{
     });
 
     const start = req.query.start;
-    let startPlaylistAt = [];
-    if (start) startPlaylistAt = ["--playlist-start",start]
+    const end = req.query.end;
+    const max = req.query.max || 3;
+
+    let filterplaylist = ['--max-downloads',max];
+
+    if (start) filterplaylist = [...filterplaylist,"--playlist-start",start]
+    if (end) filterplaylist = [...filterplaylist,"--playlist-end",end]
+    
     try{
-        yt_dl= execa('youtube-dl',[...startPlaylistAt, "-x","--no-progress","-o","./mp3/%(title)s.%(ext)s","--no-warnings", "--audio-format","mp3",`https://www.youtube.com/playlist?list=${req.params.id}`])
+        yt_dl= execa('youtube-dl',[...filterplaylist, "-x","--no-progress","-o","./mp3/%(title)s.%(ext)s","--no-warnings", "--audio-format","mp3",`https://www.youtube.com/playlist?list=${req.params.id}`])
       //  youtube-dl -x -o './mp3/%(title)s.%(ext)s' --no-warnings --audio-format mp3 https://www.youtube.com/playlist?list=PLETIo5u_JSiOrahkO8xY5tvzh8O4PLJA1
         yt_dl.stdout.pipe(stdoutFile);
         console.log('=================> im here playlist : '+req.params.id+' <==============');
@@ -78,9 +83,10 @@ app.get('/playlist/:id',async (req,res)=>{
 });
 
 app.all('/download',async (req,res)=>{
-    res.on('finish',async ()=>{
+    res.on('finish',()=>{
         console.log("finished download request.......")
-    }).on('close',()=>{ 
+    }).on('close',async ()=>{
+        await execa('rm', ['-rf','./mp3']);
         console.log('closed Download Request...')
     })
     console.log("download query params",req.query)
@@ -99,6 +105,9 @@ app.all('/download',async (req,res)=>{
         res.status(400).json(ex.message);
     }
 });
+
+
+
 app.all('/end',async (req,res)=>{
     try{
         console.log("in END ==>",yt_dl);
